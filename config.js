764ff31ad93d33563d1bb6da0f5f46a882135121
Dewxa <Kitzudew2006@gmail.com>
2024-09-22 01:43:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "aCYEwSQI#qCJRqB3_L5HTBe2opIFHlP1Ww9GtlaQX06O-DSCnPFA",
+MONGODB: process.env.MONGODB || "mongodb+srv://kitzudew2006:Dewanjana2023@cluster0.0ov7l.mongodb.net/",//enter mongo db url
 };
